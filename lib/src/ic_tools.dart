30b24a004247b3baa209430f7b7a58033fb863cf
Levi. <levifeldman37@gmail.com>
2024-09-04 18:26:44 -0400

    better

--- lib/src/ic_tools.dart
+++ lib/src/ic_tools.dart
@@ -693,6 +693,8 @@ check_time_is_less_than_5_minutes_old_in_certificate_tree(List certificate_tree)
 
 // will throw if signature is invalid
 Future<void> verify_query_signatures(Principal fective_canister_id, Uint8List quest_id, Map sponse) async {
+    SubnetData subnet_data = await get_subnet_data_for_canister(fective_canister_id);
+
     if ((sponse['signatures'] as List).isEmpty) {
         throw Exception('There must be at least 1 query response signature.');
     }
@@ -706,7 +708,7 @@ Future<void> verify_query_signatures(Principal fective_canister_id, Uint8List qu
         }
         
         Uint8List? node_public_key_DER;
-        for (NodeData node_data in (await get_subnet_data_for_canister(fective_canister_id)).nodes) {
+        for (NodeData node_data in subnet_data.nodes) {
             if (node_data.node_id == node_id) {
                 node_public_key_DER = node_data.public_key_DER;
                 break;
